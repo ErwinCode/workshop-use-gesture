506e5e3e10a99d895c95acdf907419c770ab6dc7
Erwin Haers <ehaers@kaliber.net>
2022-11-04 15:59:13 +0100

    avemekajo movement

--- src/App.universal.js
+++ src/App.universal.js
@@ -1,12 +1,39 @@
+import { useSpring, animated } from '@react-spring/web'
 import pit from '/images/avocados/pit.svg'
+import { useDrag } from '@use-gesture/react'
+
 import styles from './App.css'
 
 export default function App() {
+  const [spring, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
+  const bind = useDrag(handleDrag, {
+    from: () => [spring.x.get(), spring.y.get()],
+    bounds: { left: -250, right: 250, top: -250, bottom: 250 },
+    rubberband: true,
+  })
+  const [activeAvocado, setActiveAvocado] = React.useState(false)
+
   return (
     <div className={styles.component}>
-      <div className={styles.avocado}>
-        <img className={styles.pit} src={pit} alt='' />
+      <div className={cx(styles.avocado, activeAvocado && styles.scaredAvocado)}>
+        <animated.img
+          className={styles.pit}
+          src={pit}
+          alt=''
+          style={spring}
+          draggable='false'
+          {...bind()}
+        />
       </div>
     </div>
   )
+  function handleDrag({ offset: [x, y], down, distance }) {
+    api.start({
+      y: down ? y : 0,
+      x: down ? x : 0,
+      scale: down ? 1.5 : 1,
+    })
+    console.log(distance)
+    setActiveAvocado(distance[0] > 10 && down)
+  }
 }
